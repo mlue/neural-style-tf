593064cf4c6a07fd4e221fb4e657655c156276c6
Mathieu Lue <mlue@primary.com>
2018-03-20 12:35:41 -0400

    images

--- stylize_image.sh
+++ stylize_image.sh
@@ -3,42 +3,25 @@ set -e
 cr=`echo $'\n.'`
 cr=${cr%.}
 
-if [ "$#" -le 1 ]; then
-   echo "Usage: bash stylize_image.sh <path_to_content_image> <path_to_style_image>"
-   exit 1
-fi
-
-echo ""
-read -p "Did you install the required dependencies? [y/n] $cr > " dependencies
-
-if [ "$dependencies" != "y" ]; then
-  echo "Error: Requires dependencies: tensorflow, opencv2 (python), scipy"
-  exit 1;
-fi
-
-echo ""
-read -p "Do you have a CUDA enabled GPU? [y/n] $cr > " cuda
-
-if [ "$cuda" != "y" ]; then
-  device='/cpu:0'
-else
-  device='/gpu:0'
-fi
-
 # Parse arguments
 content_image="$1"
 content_dir=$(dirname "$content_image")
 content_filename=$(basename "$content_image")
+default_max_size=512
 
 style_image="$2"
 style_dir=$(dirname "$style_image" )
 style_filename=$(basename "$style_image")
+max_size=${4:-$default_max_size}
 
 echo "Rendering stylized image. This may take a while..."
 python neural_style.py \
---content_img "${content_filename}" \
---content_img_dir "${content_dir}" \
---style_imgs "${style_filename}" \
---style_imgs_dir "${style_dir}" \
---device "${device}" \
---verbose;
+       --content_img "${content_filename}" \
+       --content_img_dir "${content_dir}" \
+       --style_imgs "${style_filename}" \
+       --style_imgs_dir "${style_dir}" \
+       --device '/gpu:0' \
+       --style_weight "$3" \
+       --verbose \
+       --max_size "$4" \
+&& imgur-uploader --image image_output/result/result.png;
